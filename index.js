4a029dbee527818832ba6602d2c9b166aee151f9
rus <kipnis.rus@gmail.com>
2022-01-09 18:26:32 -0500

    added ability to color single cell

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ let removeColumn = document.getElementById("removeCol").addEventListener("click"
 
 let changeColor = document.getElementById("colors").addEventListener("change", (event) => {
    let newT = document.querySelector(".table");
-  if("red" === event.target.value)
+   if("" === event.target.value)
+   {
+     newT.style.backgroundColor = null;
+   }
+   if("red" === event.target.value)
     {
       for(let i = 0; i < table.rows.length; i++)
         {
@@ -60,3 +64,91 @@ let changeColor = document.getElementById("colors").addEventListener("change", (
     }
   })
 
+  // let hoverColorChange = document.getElementById("colors").addEventListener("click", (event) => {
+  //   //let hoverChange = document.querySelector(".table").addEventListener("mouseover");
+  //   if("" === event.target.value)
+  //   {
+  //     newT.style.backgroundColor = null;
+  //   }
+  //   if("red" === event.target.value)
+  //    {
+  //      for(let i = 0; i < table.rows.length; i++)
+  //        {
+  //          //let newCol = table.rows[i].insertCell(-1);
+  //          newT.style.backgroundColor = "red";
+  //        }
+  //    }
+  //    if("green" === event.target.value)
+  //    {
+  //      hoverChange = 
+  //      }
+  //    }
+  //    if("blue" === event.target.value)
+  //    {
+  //      newT.style.backgroundColor = 'blue';
+  //    }
+  //    if("orange" === event.target.value)
+  //    {
+  //      newT.style.backgroundColor = 'orange';
+  //    }
+  //  })
+   
+let hoverColorChange = document.getElementById("colors2").addEventListener("change", (event) => {
+  let row = document.getElementsByTagName('td');   
+  if(event.target.value == "red")
+        {
+          for(let i = 0 ; i < row.length; i++)
+              { 
+                row[i].onmouseup = function() {
+                  this.style.backgroundColor = 'red';
+              }
+          }
+        }
+        if(event.target.value == "green")
+        {
+          for(let i = 0 ; i < row.length; i++)
+              { 
+                row[i].onmouseup = function() {
+                  this.style.backgroundColor = 'green';
+              }
+            }
+          }
+        if(event.target.value == "blue")
+        {
+          for(let i = 0 ; i < row.length; i++)
+              { 
+                row[i].onmouseup = function() {
+                  this.style.backgroundColor = 'blue';
+              }
+            }
+          }
+        if(event.target.value == "orange")
+        {
+          for(let i = 0 ; i < row.length; i++)
+              { 
+                row[i].onmouseup = function() {
+                  this.style.backgroundColor = 'orange';
+              }
+            }
+          }
+        
+      //   if(event.target.value == "red")
+      //   {
+      //     let td = document.querySelector(".tr").addEventListener("click");
+      //     event.td.style.backgroundColor = "red";
+      //   }
+      // } 
+      
+        // if(event.target.value === "red")
+        //   {
+        //     table.onmouseover.style.backgroundColor = "red";
+        //     // if(0 == table.rows.length)
+        //     // hoverColorChange.rows[i].style.backgroundColor = "red";
+        //     // }
+        //   }
+
+
+})
+  
+   
+  
